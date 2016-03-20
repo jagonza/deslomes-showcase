7fa5e1bd66128d50bd0b4c35858fd8237f186300
Yo González <bigjagonza2000@gmail.com>
2016-03-20 13:29:47 +0100

    Initial Commit

---
+++ devslopes-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-showcase
+//
+//  Created by Javier González Rojo on 20/3/16.
+//  Copyright © 2016 Javier González Rojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
